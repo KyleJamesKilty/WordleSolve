944c56677c6c00a820e5889982f44e53cf58304b
KyleJamesKilty <98062591+KyleJamesKilty@users.noreply.github.com>
2022-03-14 10:15:08 -0500

    Fixed Version of Solver

---
+++ Solver.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 07:14:47 2022
+
+
+@author: Kyle
+"""
+from Wordle import data
+
+dicOne = {}
+dicTwo = {}
+Three = []
+notDone = 1
+
+while notDone:
+    finalList = []
+    corPos = 1
+    cor = 1
+    FalseLet = 1
+    
+    while corPos:
+        corPos = input('Letter in Correct Position: ')
+        if corPos == '0':
+            break
+        if corPos:
+            Pos = input('Position Index: ')
+            dicOne[corPos] = int(Pos)
+    while cor:
+        cor = input('Letter in Incorrect Postiion: ')
+        if cor == '0':
+            break
+        if cor:
+            IncPos = input('Position Index: ')
+            IncPos = int(IncPos)
+            if IncPos not in dicOne:
+                if IncPos not in dicTwo:
+                    dicTwo[cor] = [IncPos]
+                else:
+                    dicTwo[cor].append(IncPos)           
+    while FalseLet:
+        FalseL = input('Letter not in Word: ')
+        if FalseL == '0':
+            break
+        Three.append(FalseL)
+        
+    for word,value in data.items():
+        count = 0
+        if value == 0:
+            continue
+        for letter,pos in dicOne.items():
+            if word[pos] != letter:
+                data[word] = 0
+                break
+        for letter,poses in dicTwo.items():
+            for pos in poses:
+                if word[pos] == letter:
+                    data[word] = 0
+                    break
+        for letter in word:
+            if letter in Three:
+                data[word] = 0
+                break
+            if letter in dicTwo:
+                count += 1
+        if count != len(dicTwo):
+            data[word] = 0
+        else:
+            if data[word] == 1:
+                finalList.append(word)
+    print(finalList)
