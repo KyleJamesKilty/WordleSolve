1828a4d590957408e3287f9d6528c788694106eb
KyleJamesKilty <98062591+KyleJamesKilty@users.noreply.github.com>
2022-03-14 10:11:49 -0500

    Added Folder

---
+++ Wordle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 08:46:29 2022
+
+@author: Kyle
+"""
+
+import json
+
+with open("fiveletterword.json","r") as fiveletterwords:
+    data = json.load(fiveletterwords)
